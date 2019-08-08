7fcfc01f2fa7f87ad51ec5f8b37115d78d8f91af
David Bartolomé <David.Bartolome@nordnet.se>
2019-08-08 14:20:06 +0200

    changes

---
+++ cypress/integration/Login/login.spec.js
@@ -0,0 +1,9 @@
+describe("Login Parabank", () => {
+  it("Visit the website", () => {
+    cy.visit("https://parabank.parasoft.com/parabank/index.htm");
+  });
+
+  it("Visit the website", () => {
+    cy.visit("/");
+  });
+});
